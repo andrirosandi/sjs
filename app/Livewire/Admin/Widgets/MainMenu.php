4375d395ad6ebe93ef23f5db77313c68c5126852
andri rosandi <andrirosandi1092@gmail.com>
2024-09-02 08:58:55 +0700

    update - perbaikan menu

--- app/Livewire/Admin/Widgets/MainMenu.php
+++ app/Livewire/Admin/Widgets/MainMenu.php
@@ -33,6 +33,7 @@ class MainMenu extends Component
         ),true);
         
         
+        
 
 
     }
--- resources/views/livewire/admin/widgets/main-menu.blade.php
+++ resources/views/livewire/admin/widgets/main-menu.blade.php
@@ -1,8 +1,9 @@
 <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-slate-50 border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
     <div class="h-full px-3 pb-4 overflow-y-auto bg-slate-50 dark:bg-gray-800">
        <ul class="space-y-2 font-medium">
+         {{-- @dump($menu); --}}
 
-         @if (@$menu[0] !== null)
+         @if (!empty($menu))
             
          @foreach ($menu as $m)
          @if ($m['type']==0)
